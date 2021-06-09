c9ff2056d4519300609ac69753c14adb21ad71a4
Mohammed Hawamdeh <hawamdeh.m7md@gmail.com>
2021-06-10 01:40:22 +0300

    base project structure

---
+++ app/src/main/java/com/worldweatheronline/android/Constants.java
@@ -0,0 +1,6 @@
+package com.worldweatheronline.android;
+
+public class Constants {
+    public static final int MAX_REQUESTS_ALLOWED=500;
+    public static final String DATE_FORMAT= "MMM d, h:mm a";
+}
